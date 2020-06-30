e7fe6af610f7ad4c86805355731c65d7e7243ead
Mexson Fernandes <robomex2020@gmail.com>
2020-06-30 22:55:52 +0530

    fix navbar collapse

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,12 +1,14 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-white fixed-top nav-down navbar-transparent" color-on-scroll="500">
+  <nav class="navbar navbar-expand-lg bg-white nav-down" color-on-scroll="500">
     <div class="container">
       <div class="navbar-translate">
-        <a href="/"><img src="../assets/pswave.png" width="150px"/></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-bar bar1"></span>
-          <span class="navbar-toggler-bar bar2"></span>
-          <span class="navbar-toggler-bar bar3"></span>
+        <a class="navbar-brand" href="/" rel="tooltip" title="Paper Kit 2 PRO" data-placement="bottom">
+          <img src="../assets/pswave.png" width="150px"/>
+        </a>
+        <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+          <span style="background: #66615B;" class="navbar-toggler-bar  bar1"></span>
+          <span style="background: #66615B;" class="navbar-toggler-bar bar2"></span>
+          <span style="background: #66615B;" class="navbar-toggler-bar bar3"></span>
         </button>
       </div>
       <div class="collapse navbar-collapse"  data-color="orange">
